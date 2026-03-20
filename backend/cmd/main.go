2e4cd2c2e2b02f8dda5b5a9384d9db9102138ab2
ochamekan <yanikidev@gmail.com>
2026-03-20 19:52:40 +0200

    backend: fix graceful shutdown

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 	"syscall"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
@@ -25,18 +26,16 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT)
+	dbpool, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		logger.Fatal("Unable to create connection pool", zap.Error(err))
+	}
+	defer dbpool.Close()
 
-	var wg sync.WaitGroup
-	wg.Go(func() {
-		s := <-sigChan
-		logger.Info("Received signal, attempting graceful shutdown", zap.Stringer("signal", s))
-		cancel()
-		logger.Info("Gracefully stopped network devices service")
-	})
-
+	// repo := repository.New(dbpool)
+	// handler := handlers.New(repo)
 	mux := http.NewServeMux()
+	srv := &http.Server{Addr: ":8000", Handler: mux}
 
 	mux.HandleFunc("POST /devices", func(w http.ResponseWriter, r *http.Request) {})
 	mux.HandleFunc("GET /devices", func(w http.ResponseWriter, r *http.Request) {})
@@ -44,6 +43,22 @@ func main() {
 	mux.HandleFunc("POST /devices/{id}", func(w http.ResponseWriter, r *http.Request) {})
 	mux.HandleFunc("DELETE /devices/{id}", func(w http.ResponseWriter, r *http.Request) {})
 
-	logger.Fatal(http.ListenAndServe(":8000", mux).Error())
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT)
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		s := <-sigChan
+		logger.Info("Received signal, attempting graceful shutdown", zap.Stringer("signal", s))
+		cancel()
+		if err := srv.Shutdown(context.Background()); err != nil {
+			logger.Error("Shutdown error", zap.Error(err))
+		}
+		logger.Info("Gracefully stopped network devices service")
+	})
 
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Fatal("ListenAndServe error", zap.Error(err))
+	}
+
+	wg.Wait()
 }
